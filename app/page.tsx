b1763d848af9f84584d83bd26359ce1005e67bd8
TimiMaliki <malikitimi@gmail.com>
2026-09-23 10:54:42 +0100

    added :components for twoStraightLines

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import TwoStraightLines from "./component/TwoStraightLines";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-white">
-   
-    </div>
+    <>
+   <TwoStraightLines/>
+    </>
   );
 }
