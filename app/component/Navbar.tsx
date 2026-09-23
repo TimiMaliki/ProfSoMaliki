ae852327755fcd04635f96bce493304a839ce8fe
TimiMaliki <malikitimi@gmail.com>
2026-09-23 11:14:27 +0100

    added:Navbar

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -2,27 +2,21 @@ import React from 'react'
 import logo from "../images/dadsLogo.png"
 type Props = {}
 
-function Navbar({}: Props) {
-  return (
-    <div className='flex  justify-between'>
-
-        {/* Logo */}
-
-  <div className="logo -mt-24 text-start  w-1/2 p-12"><img src={logo.src} alt="Logo" className="h-16 w-16" /></div>
-
- {/* menus */}
-
-  <div className="menu  -mt-12 text-start  w-1/2 ">
-    <ul className='grid grid-cols-1 gap-1 place-content-center text-center'>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </div>
-
-
-    </div>
-  )
+function Navbar({ }: Props) {
+    return (
+        <div className='flex  justify-between'>
+            {/* Logo */}
+            <div className="logo -mt-24 text-start  w-1/2 p-12"><img src={logo.src} alt="Logo" className="h-16 w-16" /></div>
+            {/* menus */}
+            <div className="menu  -mt-12 text-start  w-1/2 ">
+                <ul className='grid grid-cols-1 gap-1 place-content-center text-center'>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
--- app/component/TwoStraightLines.tsx
+++ app/component/TwoStraightLines.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 type Props = {}
 
-function TwoStraightLines({}: Props) {
+function TwoStraightLines({ }: Props) {
   return (
     <div className='py-10 md:py-14 flex  items-center justify-center gap-10 p-2 md:p-10'>
- <div className="long-line w-600 md:w-500 border bg-black"></div>
-  <div className="long-line w-200 border bg-black"></div>
+      <div className="long-line w-600 md:w-500 border bg-black"></div>
+      <div className="long-line w-200 border bg-black"></div>
     </div>
   )
 }
