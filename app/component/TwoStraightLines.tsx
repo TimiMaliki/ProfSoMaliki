637c1c2159f6f14a60ff1c6f66a2db1d3d141046
TimiMaliki <malikitimi@gmail.com>
2026-09-23 10:55:09 +0100

    added :components for twoStraightLines

--- app/component/TwoStraightLines.tsx
+++ app/component/TwoStraightLines.tsx
@@ -5,12 +5,8 @@ type Props = {}
 function TwoStraightLines({}: Props) {
   return (
     <div className='py-10 md:py-24 flex  items-center justify-center gap-10 p-2 md:p-10'>
- <div className="long-line w-600 md:w-500 border bg-black">
-
- </div>
-  <div className="long-line w-200 border bg-black">
-
- </div>
+ <div className="long-line w-600 md:w-500 border bg-black"></div>
+  <div className="long-line w-200 border bg-black"></div>
     </div>
   )
 }
